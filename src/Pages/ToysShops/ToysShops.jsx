9ef51a797294f070d9fbcb26712f5669755d4b18
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-19 02:39:45 +0600

    sub category toys rating add from react rating component

--- src/Pages/ToysShops/ToysShops.jsx
+++ src/Pages/ToysShops/ToysShops.jsx
@@ -1,4 +1,5 @@
-
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const ToysShops = ({toy}) => {
 
@@ -8,12 +9,20 @@ const ToysShops = ({toy}) => {
         <div className="">
         <img className="w-64 h-64 shadow-lg rounded shadow-gray-400 bg-gray-700" src={image} alt="" />
         </div>
-        <div className="space-y-2 px-10 sm:pb-5">
+        <div className="space-y-3 px-10 sm:pb-5">
         <h2 className="text-2xl px-2 font-bold">{name}</h2>
         <p className="text-xl px-2 font-semibold text-orange-500">Price: ${price}</p>
-        <p className="px-2">{rating}</p>
+
+        <div className="flex items-center px-2">
+        <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+        <span className='ms-2'>{rating}</span>
+        </div>
+
         <button className="btn btn-sm btn-outline btn-accent">View Details</button>
         </div>
+
+
+        
 
 
 
