ba132b29c92a1caaca5486b00c354c894f33e4b7
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-20 01:27:28 +0600

    click the delete button toy will be remove from the list , asked confirm delete confirmation

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-
+import Swal from 'sweetalert2'
 import { AuthContext } from '../../Providers/AuthProvider';
 import MyToysDetails from '../MyToysDetails/MyToysDetails';
 
@@ -15,6 +15,41 @@ const MyToys = () => {
         .then(res => res.json())
         .then(data => setMyToys(data))
     }, [])
+
+    const handleDelete = id => {
+        const proceedDel = confirm;
+        if(proceedDel){
+            fetch(`http://localhost:5000/addtoys/${id}`, {
+                method: 'DELETE',
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0 ){
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You won't be able to delete this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                      }).then((result) => {
+                        if (result.isConfirmed) {
+                          Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                          )
+                        }
+                      })
+                    const remaining = myToys.filter(toys => toys._id !== id);
+                    setMyToys(remaining);
+                }
+            })
+        }
+    }
+
     return (
         <div>
             <h3 className='text-4xl font-bold text-center text-gray-600 m-10'>My Toys</h3>
@@ -23,17 +58,14 @@ const MyToys = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+                            
                             <th>Image</th>
                             <th>Toys Name</th>
                             <th>Seller Name</th>
                             <th>Price</th>
                             <th>Quantity</th>
                             <th>Update</th>
+                            <th>Delete</th>
 
                             <th></th>
                         </tr>
@@ -43,6 +75,7 @@ const MyToys = () => {
                             myToys.map(toys => <MyToysDetails
                             key={toys._id}
                             toys={toys}
+                            handleDelete={handleDelete}
                             ></MyToysDetails>)
                         }
                     </tbody>
--- src/Pages/MyToysDetails/MyToysDetails.jsx
+++ src/Pages/MyToysDetails/MyToysDetails.jsx
@@ -1,30 +1,31 @@
 
+import { FaTrashAlt, FaUserCircle, FaImages } from 'react-icons/fa';
 
-const MyToysDetails = ({ toys }) => {
-    const { email, photo, name, seller, price, quantity } = toys;
+
+
+const MyToysDetails = ({ toys, handleDelete }) => {
+    const { _id, email, photo, name, seller, price, quantity } = toys;
+
+    
+
     return (
-
         <tr>
-            <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-            </th>
             <td>
-                <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src={photo} alt="" />
-                        </div>
+            <div className="avatar">
+                    <div className="rounded-lg w-14 h-14">
+                        {photo && <img src={photo} alt="Avatar" />}
+                        {!photo && <FaImages className='w-14 h-14' />}
                     </div>
-                    
                 </div>
             </td>
             <td>{name}</td>
             <td>{seller}</td>
             <td>{price}</td>
             <td>{quantity}</td>
+            
             <th><button className="btn btn-sm btn-outline btn-accent">Update</button></th>
+
+            <td><span onClick={() => handleDelete(_id)} className='text-3xl text-red-600 '><FaTrashAlt /></span></td>
         </tr>
 
     );
